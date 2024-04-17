ccce84038e6c05a8a21fa77e3730d8aa4604d96a
NarendraReddy <narendra.nya@gmail.com>
2024-04-17 15:30:35 -0700

    03-variable-commit

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=RAMESH
+PERSON2=SURESH
+
+echo "$PERSON1::Hi $PERSON2,how are you"
+echo "$PERSON2::Hi $PERSON1,I am fine,how are you"
+echo "$PERSON1:: i am good,how is your job"
+echo "$PERSON2::Good.planning to take devops course"
+
