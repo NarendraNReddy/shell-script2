279aa6e6c542064c1b10dcb9d507933302919600
NarendraReddy <narendra.nya@gmail.com>
2024-04-17 15:32:08 -0700

    04-variable-commit

---
+++ 04-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1::Hi $PERSON2,how are you"
+echo "$PERSON2::Hi $PERSON1,I am fine,how are you"
+echo "$PERSON1:: i am good,how is your job"
+echo "$PERSON2::Good.planning to take devops course"
+
