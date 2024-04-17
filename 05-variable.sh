d329a845e96a209385f707244b352101b0a56f0a
NarendraReddy <narendra.nya@gmail.com>
2024-04-17 15:34:55 -0700

    05-variable-commit2

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
 echo "Enter user name:"
-read USERNAME 
+read -s USERNAME 
 
 echo "Enter password:"
-read PASSWORD
+read -s PASSWORD
 
 echo "User name is $USERNAME,password is $PASSWORD"
