a995b8cf8af039cc97546d21dcdcabe5018ccea1
NarendraReddy <narendra.nya@gmail.com>
2024-04-18 14:59:11 -0700

    mysql

--- MySQL.sh
+++ MySQL.sh
@@ -3,8 +3,13 @@
 USERID=$(id -u)
 SCRIPT_NAME=$(echo $0 |  cut -d "." -f1)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-echo $TIMESTAMP
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+BL="\e[34m"
+N="\e[0m"
 
 if [ $USERID -ne 0 ];
 then 
@@ -13,6 +18,29 @@ then
 else 
     echo "You are super user"    
 fi    
+
+VALIDATE()
+{
+    if [ $1 - ne 0 ];
+    then 
+        echo -e "$2 is ...$R FAILURE $N"
+    else 
+        echo "$2 is $G SUCCESS $N"    
+}
+
+dnf install mysql-server -y &>>$LOGFILE 
+VALIDATE $? "MySQL server"
+
+systemctl enable mysqld -y &>>$LOGFILE
+VALIDATE $? "Enable mysqld server"
+
+systemctl start mysqld -y &>>$LOGFILE
+VALIDATE $? "Start mysqld server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+VALIDATE $? "Setting Up MySQL server"
+
+
 
 
 
