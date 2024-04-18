f4a486bb47d1c34421cf1af0e382274fe095bfe5
NarendraReddy <narendra.nya@gmail.com>
2024-04-18 06:50:48 -0700

    11-installation-addlogs

--- 13-installations.sh
+++ 13-installations.sh
@@ -11,6 +11,18 @@ N="\e[0m"
 
 USERID=$(id -u)
 
+VALIDATE()
+{
+   if [ $1 -ne 0 ];
+   then
+        echo -e "$2 is .... $R FAILURE $N"
+        exit 1
+   else
+        echo -e "$2 is ..... $G SUCCESS $N"
+   fi              
+
+}
+
 if [ $USERID -ne 0 ];
 then
     echo "Not super user"
