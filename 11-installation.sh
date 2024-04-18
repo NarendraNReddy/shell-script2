357e249721c3a6467cbc5152350bbc7a977df3ab
NarendraReddy <narendra.nya@gmail.com>
2024-04-18 05:33:06 -0700

    11-installation-addlogs

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TIMESTAMP=$(date +%F-%H-%M-%s)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=tmp/$SCRIPT_NAME-$TIMESTAMP.log
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 
 ID1=$(id -u)
