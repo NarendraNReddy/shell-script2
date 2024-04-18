20c9cfdee181577a70ebde6bdcfb08ebe0ffd331
NarendraReddy <narendra.nya@gmail.com>
2024-04-18 06:39:36 -0700

    11-installation-addlogs

--- 13-installations.sh
+++ 13-installations.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+B="\e[33m"
+BL="\e[34m"
+N="\e[0m"
 
 USERID=$(id -u)
 
@@ -13,5 +22,5 @@ fi
 for i in $@;
 do
     echo "packages to install $i"
-    dnf list installed $i &>>$LOGFILE 
+    dnf list installed $i &>>$LOGFILE
 done
