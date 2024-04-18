72d5d5343f310ea21cd345575525326c34ece191
NarendraReddy <narendra.nya@gmail.com>
2024-04-18 05:51:22 -0700

    11-installation-addlogs

--- 11-installation.sh
+++ 11-installation.sh
@@ -3,17 +3,22 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
-
+R="\e[31m"
+G="\e[32m"
+B="\e[33m"
+BL="\e[34m"
+N="\e[0m"
+echo "Script started:$TIMESTAMP"
 ID1=$(id -u)
 
 VALIDATE()
 {
    if [ $1 -ne 0 ];
    then
-        echo "$2 is .... FAILURE"
+        echo "$2 is .... $R FAILURE $N"
         exit 1
    else
-        echo "$2 is ..... SUCCESS"
+        echo "$2 is ..... $G SUCCESS $N"
    fi              
 
 }
